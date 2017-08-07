93ca3b9d4775cf63dcdbe9ec9e335ab768b3e16f
abhinavGirish <abhinav.girish97@gmail.com>
2017-08-07 18:53:18 -0400

    test commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Inside the App class</div>
+      <div>Making a change here...</div>
     );
   }
 }
