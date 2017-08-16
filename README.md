a38c9f0d210bdfd023bdea6504d964c7afc64285
abhinavGirish <abhinav.girish97@gmail.com>
2017-08-16 00:35:52 -0400

    adding in video playlist components

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # react-project
-my own React project
+a site detailing information on star wars filmography - more details to come soon!
 
-#instructions
+
+# instructions
 run 'npm start' in the terminal 
 go to your browser and type in 'localhost:8080' in the address to render the application
+
+project based off of the React-Redux tutorial by Stephen Grider
+
+# technology stack/APIs to be used
+React.js
+The Star Wars API (swapi.co)
+Neo4j
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import VideoItem from './components/video_list_item';
+const API_KEY = 'AIzaSyCMuWciCAHo9biuMJty5cPT2PEZafpFWw8'; 
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+   constructor(props){
+     super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+     this.state = { 
+        videos: [],
+        selectedVideo: null
+    };
+
+     this.videoSearch('The Last Jedi');
+
+   }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({ 
+            videos: videos,
+            selectedVideo: videos[0]
+         }); 
+     });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HMTL and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); 
